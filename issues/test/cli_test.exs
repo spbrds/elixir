c5dc36aa6f41566311835e13fbcd52dbe2b1a6b2
spbrds <spbrds@gmail.com>
2016-12-19 23:55:41 +0000

    katas challanges

--- issues/test/cli_test.exs
+++ issues/test/cli_test.exs
@@ -10,7 +10,17 @@ test "testing help on parse args" do
 end
 
 test "testing author on parse args" do 
-	assert parse_args(["--author"]) == "spbrds"
+	assert parse_args(["--author"]) == {:author,"This code is developed by spbrds"}
 end
+
+test "testing the three full arguments" do
+	assert parse_args(["spbrds","project_elixir","10"]) == {"spbrds","project_elixir",10}
+end
+
+test "without the number of issues" do
+	assert parse_args(["spbrds","project_elixir"]) == {"spbrds","project_elixir",4}
+end
+
+#test "test the git fetching" do end
 
 end
