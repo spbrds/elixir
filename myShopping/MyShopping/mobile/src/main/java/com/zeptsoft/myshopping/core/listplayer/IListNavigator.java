91399500e3ca62b1e5b5e69bf968314e584676b5
spbrds <spbrds@gmail.com>
2017-03-23 00:10:00 +0000

    navigation module succeeded

--- myShopping/MyShopping/mobile/src/main/java/com/zeptsoft/myshopping/core/listplayer/IListNavigator.java
+++ myShopping/MyShopping/mobile/src/main/java/com/zeptsoft/myshopping/core/listplayer/IListNavigator.java
@@ -1,18 +1,8 @@
 package com.zeptsoft.myshopping.core.listplayer;
 
-import com.zeptsoft.myshopping.datatypes.Item;
-
-import java.util.List;
-
 /**
- * Created by SSBook on 16/03/17.
+ * Created by SSBook on 22/03/17.
  */
 
 public interface IListNavigator {
-    public Item next();
-    public Item previous();
-    public void check();
-    public void startNavigation(List<Item> list, String id);
-
-
 }
