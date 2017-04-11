ea97708a6d3a9cc587aed56df19b3cedc0439a14
spbrds <spbrds@gmail.com>
2017-04-12 00:06:49 +0100

    refactoring to include fragments

--- myShopping/MyShopping/mobile/src/main/java/com/zeptsoft/myshopping/core/fragments/ItemListFragment.java
+++ myShopping/MyShopping/mobile/src/main/java/com/zeptsoft/myshopping/core/fragments/ItemListFragment.java
@@ -1,16 +1,14 @@
-package com.zeptsoft.myshopping;
-
+package com.zeptsoft.myshopping.core.fragments;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
@@ -19,19 +17,21 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
+import com.zeptsoft.myshopping.R;
 import com.zeptsoft.myshopping.core.MyShoppingApplication;
 import com.zeptsoft.myshopping.core.adapters.ItemListAdapter;
 import com.zeptsoft.myshopping.core.listmanager.IListManager;
 import com.zeptsoft.myshopping.core.listmanager.ListManager;
-import com.zeptsoft.myshopping.core.listmanager.MockListManager;
 import com.zeptsoft.myshopping.datatypes.Item;
 import com.zeptsoft.myshopping.notification.NotificationHelper;
 
+/**
+ * Created by SSBook on 10/04/17.
+ */
 
+public class ItemListFragment extends Fragment{
 
-
-public class MainActivity extends AppCompatActivity {
-
+    private static final String LIST_ID_EXTRA_ID = "com.myshopping.list_id";
     private Animation upAnimation;
     private Animation downAnimation;
     private Animation addRotation;
@@ -39,116 +39,97 @@ public class MainActivity extends AppCompatActivity {
 
     private boolean addOpened = false;
 
+    private LinearLayout addLayout;
+    private ImageButton addButton;
+
     private LinearLayout subActionView;
     private RecyclerView itemRecyclerView;
     private ItemListAdapter adapter;
     private IListManager listManager;
-
-    private LinearLayout addLayout;
-
-    private ImageButton addButton;
     private Button addItemButton;
-
     private EditText itemNameEdit;
     private EditText itemCategoryEdit;
     private EditText itemObservationsEdit;
     private CheckBox itemUrgentCheck;
 
 
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View fragmentView = inflater.inflate(R.layout.item_list_layout, container);
 
-        setContentView(R.layout.activity_main);
-       // subActionView = (LinearLayout)findViewById(R.id.sub_action_container);
+        initLayout(fragmentView);
 
-        //initiating listManager
+        return fragmentView;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        //initiating ListManager
         listManager = new ListManager("por aqui id");
 
-        itemRecyclerView = (RecyclerView)findViewById(R.id.productList);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    }
 
-        adapter = new ItemListAdapter(this, listManager.getList());
+    private void initLayout(View v){
+        itemRecyclerView = (RecyclerView)v.findViewById(R.id.productList);
+        adapter = new ItemListAdapter(this.getActivity(), listManager.getList());
         itemRecyclerView.setAdapter(adapter);
-        itemRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-
-
-        addLayout = (LinearLayout) findViewById(R.id.add_layout);
+        itemRecyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
+        addLayout = (LinearLayout) v.findViewById(R.id.add_layout);
 
         //updateListView();
-        registerButtonsListener();
+        registerButtonsListener(v);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void registerButtonsListener(){
-      /*  //registering reload button
-        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "List reseted", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });/
-*/
-         //registering notificationButton
-        findViewById(R.id.fabNot).setOnClickListener(
+    private void registerButtonsListener(View v){
+    /*  //registering reload button
+      findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              Snackbar.make(view, "List reseted", Snackbar.LENGTH_LONG)
+                      .setAction("Action", null).show();
+          }
+      });/
+    */
+    //registering notificationButton
+    v.findViewById(R.id.fabNot).setOnClickListener(
             new View.OnClickListener(){
-                @Override
-                public void onClick(View view) {
-                    Snackbar.make(view, "Notification Created", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                    //initiating navigation
-                    MyShoppingApplication app = (MyShoppingApplication)getApplicationContext();
-                    app.getNavigator().startNavigation(listManager,"");
+        @Override
+        public void onClick(View view) {
+            Snackbar.make(view, "Notification Created", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
+            //initiating navigation
+            MyShoppingApplication app = (MyShoppingApplication)getActivity().getApplicationContext();
+            app.getNavigator().startNavigation(listManager,"");
 
-                    //showing notification
-                    buildNotification();
-                }
-            });
-
-
-        addButton = (ImageButton) findViewById(R.id.list_add);
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                animateAddLayout();
-
-            }
-        });
-
-        addItemButton = (Button)findViewById(R.id.item_add_button);
-        addItemButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                addItemToList();
-            }
-        });
+            //showing notification
+            buildNotification();
+        }
+    });
 
 
-    }
+    addButton = (ImageButton) v.findViewById(R.id.list_add);
+    addButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            animateAddLayout();
+
+        }
+    });
+
+    addItemButton = (Button) v.findViewById(R.id.item_add_button);
+    addItemButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            addItemToList();
+        }
+    });
+
+
+}
 
     private void addItemToList(){
         bindFormFields();
@@ -167,10 +148,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void bindFormFields(){
         if(itemNameEdit == null){
-            itemNameEdit = (EditText) findViewById(R.id.new_item_name);
-            itemCategoryEdit = (EditText) findViewById(R.id.new_item_category);
-            itemObservationsEdit = (EditText) findViewById(R.id.new_item_observations);
-            itemUrgentCheck = (CheckBox) findViewById(R.id.new_item_urgent_check);
+            itemNameEdit = (EditText) getView().findViewById(R.id.new_item_name);
+            itemCategoryEdit = (EditText) getView().findViewById(R.id.new_item_category);
+            itemObservationsEdit = (EditText) getView().findViewById(R.id.new_item_observations);
+            itemUrgentCheck = (CheckBox) getView().findViewById(R.id.new_item_urgent_check);
         }
     }
 
@@ -191,7 +172,7 @@ public class MainActivity extends AppCompatActivity {
     public void buildNotification(){
         //mudar o builder e o Manager para statics numa classe
         //para poder fazer o update da notificação
-        NotificationHelper.getInstance().buildNotification(this);
+        NotificationHelper.getInstance().buildNotification(this.getActivity());
 
     }
 
@@ -209,18 +190,16 @@ public class MainActivity extends AppCompatActivity {
         }
         addOpened = !addOpened;
 
-
-
     }
 
     //initing animation and setting listeners
     public void initAnimations(){
         if(addRotation == null){
-            addRotation = AnimationUtils.loadAnimation(this, R.anim.add_button_to_close);
+            addRotation = AnimationUtils.loadAnimation(this.getActivity(), R.anim.add_button_to_close);
         }
 
         if(addReset == null){
-            addReset = AnimationUtils.loadAnimation(this, R.anim.add_button_reset);
+            addReset = AnimationUtils.loadAnimation(this.getActivity(), R.anim.add_button_reset);
         }
 
         /*if(upAnimation == null){
@@ -262,6 +241,15 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }*/
+    }
+
+    public static ItemListFragment getInstance(String listId){
+        ItemListFragment fragment = new ItemListFragment();
+        Bundle args = new Bundle();
+        args.putString(LIST_ID_EXTRA_ID,listId);
+
+        fragment.setArguments(args);
+        return fragment;
     }
 
 }
